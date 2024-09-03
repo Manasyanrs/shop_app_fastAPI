5da9d96f3a30e38b52754b762b40f1ff993d2277
radik <radikmanasyan@gmail.com>
2024-09-03 13:33:58 +0400

    add db helper to main

--- shop_app/main.py
+++ shop_app/main.py
@@ -1,16 +1,28 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
-app = FastAPI(
+from core.models import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+main_app = FastAPI(
     title="Shop App",
+    lifespan=lifespan,
 )
-app.include_router(api_router)
+main_app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
